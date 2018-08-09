f8a0a11b5a2f657235eebe8eee9e996ebbd4d2af
Juan José González Basilio <jjnorris31@gmail.com>
2018-08-09 08:57:04 -0500

    Create sum_numbers.py

---
+++ sum_numbers.py
@@ -0,0 +1,12 @@
+"""
+Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
+Examples
+4 ➞ 10
+
+13 ➞ 91
+
+600 ➞ 180300
+"""
+
+def add_up(num):
+	return sum(range(1, num + 1, 1))
