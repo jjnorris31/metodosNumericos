565c58a4521f5f1f86e690fe407ea788b3dc2e90
Juan José González Basilio <jjnorris31@gmail.com>
2018-08-09 09:11:12 -0500

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,13 @@
+"""
+Create a function that takes an integer and returns the factorial of that integer. That is, the integer multiplied by all positive lower integers.
+Examples
+3 ➞ 6
+
+5 ➞ 120
+
+13 ➞ 6227020800
+"""
+import math
+
+def factorial(num):
+	return math.factorial(num)
