e6a93ee371d75258eae76e39b7d918259d4e3391
Juan José González Basilio <jjnorris31@gmail.com>
2018-08-21 00:05:37 -0500

    Create valid_pin.py

---
+++ valid_pin.py
@@ -0,0 +1,14 @@
+"""
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns True if the PIN is valid and False if it's not.
+Examples
+"1234" ➞ True
+
+"12345" ➞ False
+
+"a234" ➞ False
+
+"" ➞ False
+"""
+
+def is_valid_PIN(pin):
+	return (len(pin) == 4 or len(pin) == 6) and pin.isdigit()
